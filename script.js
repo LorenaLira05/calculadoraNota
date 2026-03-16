970220efe8f22f6f252827e869b3afb57766ebde
Lorena Lira <naheelee2005@gmail.com>
2026-03-16 15:19:45 -0300

    Fix comparison operators in calcularMedia function

--- script.js
+++ script.js
@@ -4,23 +4,22 @@ let n3 = 0.0;
 let n4 = 0.0;
 
 function calcularMedia(){
-    //n1, n2,n3,n4 = input
     mediaNotas = (n1 + n2 + n3 + n4) / 4
-    if(mediaNotas=> 7.0){
+    if(mediaNotas >= 7.0){
         console.log("Voce esta aprovado!")
     } else {
         let nrec;
         console.log("Voce esta de recuperacao")
         console.log("Digite sua nota da recuperacao")
         //nrec = input
-        if(nrec=> 7.0){
+        if(nrec >= 7.0){
             console.log("Voce esta aprovado")
         } else{
             let nfinal;
             console.log("Voce esta na final")
             console.log("Digite sua nota final")
             //nfinal = input
-            if (nfinal=> 7.0){
+            if (nfinal>= 7.0){
                 console.log("Voce esta aprovado")
             } else {
                 console.log("Voce esta reprovado")
@@ -28,5 +27,3 @@ function calcularMedia(){
         }
     }
 }
-
-calcularMedia();
