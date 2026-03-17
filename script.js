57ff1751cbe7c4756c6463606a184f529fea4a06
Lorena Lira <naheelee2005@gmail.com>
2026-03-17 13:46:55 -0300

    Refactor calcularMedia function with parameters

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-let n1 = 0.0; 
-let n2 = 0.0;
-let n3 = 0.0;
-let n4 = 0.0;
-
-function calcularMedia(){
-    mediaNotas = (n1 + n2 + n3 + n4) / 4
-    if(mediaNotas >= 7.0){
-        console.log("Voce esta aprovado!")
-    } else {
-        let nrec;
-        console.log("Voce esta de recuperacao")
-        console.log("Digite sua nota da recuperacao")
-        //nrec = input
-        if(nrec >= 7.0){
-            console.log("Voce esta aprovado")
-        } else{
-            let nfinal;
-            console.log("Voce esta na final")
-            console.log("Digite sua nota final")
-            //nfinal = input
-            if (nfinal>= 7.0){
-                console.log("Voce esta aprovado")
-            } else {
-                console.log("Voce esta reprovado")
-            }
-        }
-    }
+function calcularMedia(n1, n2, n3, n4) {
+    let mediaNotas = (n1 + n2 + n3 + n4) / 4;
+    console.log("Média inicial: " + mediaNotas.toFixed(2));
+ 
+    if (mediaNotas >= 7) {
+        console.log("Você está aprovado!");
+    } else {
+        console.log("Você está de recuperação");
+ 
+        let nrec = parseFloat(prompt("Digite sua nota da recuperação:"));
+ 
+        if (nrec >= 7) {
+            console.log("Você está aprovado!");
+        } else {
+            console.log("Você está na final");
+ 
+            let nfinal = parseFloat(prompt("Digite sua nota final:"));
+ 
+            if (nfinal >= 7) {
+                console.log("Você está aprovado!");
+            } else {
+                console.log("Você está reprovado!");
+            }
+        }
+    }
 }
+ 
+calcularMedia(5, 10, 10, 10);
